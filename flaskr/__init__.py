b8d47c993463f7edd92f4a7c0e68e89790eb507c
Noemy <pamela.messias@escolar.ifrn.edu.br>
2026-07-10 20:36:53 -0300

    Blog Blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -26,6 +27,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+
     app.add_url_rule('/', endpoint='index')
 
-    return app  
+    return app
